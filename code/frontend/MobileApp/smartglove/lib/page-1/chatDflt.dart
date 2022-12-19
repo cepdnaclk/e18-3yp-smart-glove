323b9ed6174c62e0567f90dee062b9d0fdeed8e6
Thakshila_Jayathilake <e18156@eng.pdn.ac.lk>
2022-12-19 15:04:10 +0530

    create new chat and default chats interfaces add

--- code/frontend/MobileApp/smartglove/lib/page-1/chatDflt.dart
+++ code/frontend/MobileApp/smartglove/lib/page-1/chatDflt.dart
@@ -1,14 +1,15 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/gestures.dart';
-// import 'dart:ui';
-// import 'package:google_fonts/google_fonts.dart';
-// import 'package:myapp/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+// import 'package:myapp/page-1/body_chat_default.dart';
+import 'package:myapp/utils.dart';
 
-// class ChatDefault extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: ,
-//     )
-//   }
-// }
+import 'body_chat_default.dart';
+
+class ChatDefault extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BodyChatDefault();
+  }
+}
