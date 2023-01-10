0a5c1fdce3f13ee9406ccf978610b00e330c7750
Thakshila_Jayathilake <e18156@eng.pdn.ac.lk>
2023-01-10 22:09:32 +0530

    combine chat page with earlier work

--- code/frontend/MobileApp/smartglove/lib/page-1/chatDflt.dart
+++ code/frontend/MobileApp/smartglove/lib/page-1/chatDflt.dart
@@ -5,12 +5,14 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/utils.dart';
 
 import 'body_chat_List_d.dart';
+import 'body_chat_default.dart';
 
 class ChatDefault extends StatelessWidget {
   final String text;
   const ChatDefault({Key? key, required this.text}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return bodyChatPage(text: text);
+    return BodyChatDefault(text: text);
+    // return bodyChatPage();
   }
 }
