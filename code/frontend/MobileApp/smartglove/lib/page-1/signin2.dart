fd90f706348be5b3b8541e7500a4b689dd263b6e
Thakshila_Jayathilake <e18156@eng.pdn.ac.lk>
2023-01-15 18:42:12 +0530

    connect interfaces with start

--- code/frontend/MobileApp/smartglove/lib/page-1/signin2.dart
+++ code/frontend/MobileApp/smartglove/lib/page-1/signin2.dart
@@ -1,20 +1,20 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/gestures.dart';
-// import 'dart:ui';
-// import 'package:google_fonts/google_fonts.dart';
-// import 'package:myapp/page-1/body_signin.dart';
-// import 'package:myapp/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/page-1/body_signin.dart';
+import 'package:myapp/utils.dart';
 
-// class SignIn extends StatefulWidget {
-//   const SignIn({super.key});
+class SignIn extends StatefulWidget {
+  const SignIn({super.key});
 
-//   @override
-//   // ignore: no_logic_in_create_state
-//   State<StatefulWidget> createState() {
-//     return BodySignIn();
-//   }
-//   // @override
-//   // Widget build(BuildContext context) {
-//   //   return BodySignIn();
-//   // }
-// }
+  @override
+  // ignore: no_logic_in_create_state
+  State<StatefulWidget> createState() {
+    return BodySignIn();
+  }
+  // @override
+  // Widget build(BuildContext context) {
+  //   return BodySignIn();
+  // }
+}
